cd4131f5ed33fbda0a87de07222eed691d4cde10
SamuelPaimAraujoCezar <samuka.cezar@hotmail.com>
2021-02-04 01:02:37 -0300

    Login Page do Instagram

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function downloadInApple() {
+    window.open("https://itunes.apple.com/app/instagram/id389801252?pt=428156&ct=igweb.loginPage.badge&mt=8&vt=lo")
+}
+
+function getOnGooglePlay() {
+    window.open("https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D496A4439-4798-409C-B1E6-F9B6967E10EB%26utm_content%3Dlo%26utm_medium%3Dbadge")
+}
